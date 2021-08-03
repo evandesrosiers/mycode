ff03c7aff6cfb20f4b297c5a32944aca2fc23d31
Evan DesRosiers <evandesrosiers@gmail.com>
2021-08-03 15:05:46 +0000

    join and split

---
+++ strings/stringmethod01.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+#Author Evan Sean DesRosiers, Date: 03AUG2021
+
+#create small string
+lilstring = "Alta3 Research offers classes on Python coding"
+newlist = lilstring.split(" ") # returns list
+print(newlist)
+
+#make list of strings
+myiplist = ["192", "168", "0", "12"]
+#combine with a period into singleip
+singleip = ".".join(myiplist)
+print(singleip)
+
